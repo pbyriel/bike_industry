5dc03861b2cd128eae35261493e63b86d333aa04
Peter Byriel <peter.byriel@gmail.com>
2016-07-17 15:38:59 +0000

    Developed bike production functions

--- bicycles.py
+++ bicycles.py
@@ -10,33 +10,68 @@ class Bicycle(object):
         self.brand = brand
 
 
+
 class Manufacturer(object):
-    """ Has a name (string) and a profit margin (number). 
+    """ Has a name (string), profit margin (number) and a type (string). 
         Can make_bikes, returned in list object"""
-    def __init__(self, name, margin):
+    def __init__(self, name, margin, type):
         self.name = name
         self.margin = margin
-        
-    def make_bikes(self, model, kilo, cost_per_bike, number_of_bikes):
-        bikes_produced = []
-        for i in range(number_of_bikes):
-            bikes_produced.append(Bicycle(model, kilo, cost_per_bike, brand=self.name))
-        
+        self.type = type
+
+
+    def make_bike(self, model, kilo, cost_of_bike):
+        bike_produced = Bicycle(model, kilo, cost=cost_of_bike*(1 + self.margin/100), brand=self.name)
+        return bike_produced
+
+
+
 class Sportsbikes(Manufacturer):
-    pass
-    #super for init and make_bikes, filled out variables, depending on type
+    def __init__(self):
+        super(Sportsbikes, self).__init__(name="Sporty", margin=35, type="Sports bikes manufacturer")
+        # MENTOR: I don't understand why I have to provide arguments to super
+    
+    
+    def make_fast_bike(self):
+        super(Sportsbikes, self).make_bike(model="Fast bike", kilo=6, cost_of_bike=100)
+    
+    
+    def make_racing_bike(self):
+        super(Sportsbikes, self).make_bike(model="Racing bike", kilo=5, cost_of_bike=120)
+
+
+    def make_pro_bike(self):
+        super(Sportsbikes, self).make_bike(model="Pro bike", kilo=4.5, cost_of_bike=130)
+    #Whats the best way to get these into a list?
+
+
 
 class Citybikes(Manufacturer):
-    pass 
-    #super for init and make_bikes, filled out variables, depending on type
+    def __init__(self):
+        super(Citybikes, self).__init__(name=None, margin=30, type="City bikes manufacturer")
+
+
+    def make_fast_bike(self):
+        super(Citybikes, self).make_bike(model="Urban", kilo=6, cost_of_bike=80)
+    
+    
+    def make_commuter_bike(self):
+        super(Citybikes, self).make_bike(model="Commuter bike", kilo=5, cost_of_bike=90)   
+
+
+    def make_shopper_bike(self):
+        super(Citybikes, self).make_bike(model="Shopper bike", kilo=4.5, cost_of_bike=100)
+    #Whats the best way to get these into a list?
+
 
 
 class Bike_shop(object):
-    """Bike_shop takes 3 arguments: name (string), inventory (list of Bicycle instances) and margin (as percent)"""
-    def __init__(self, name, inventory, margin=20):
+    """Bike_shop takes 4 arguments: name (string), inventory (list of Bicycle instances) and margin (as percent)"""
+    def __init__(self, name, inventory=[], capital=5000, margin=20):
         self.name = name
-        self.inventory = inventory.copy() #makes shallow copy to not affect orig. bikes
-        #should start with empty stock and working capital
+        self.inventory = inventory
+        self.capital = capital
+        self.margin = margin
         for bike in self.inventory:
             bike.salesprice = bike.cost * (1 + margin/100)
             bike.profit = bike.salesprice - bike.cost
@@ -48,18 +83,27 @@ class Bike_shop(object):
 
     def sell(self, bike):
         self.profits += bike.profit
+        self.capital += bike.salesprice
         bike.stock -= 1 
 
-        
+
     def print_status(self):
         print("Inventory status:")
         for bike in self.inventory:
             print("Model: {} - in stock: {}".format(bike.model, bike.stock))
         print("Total profits: ${}".format(round(self.profits,2)))
+        print("Working capital: ${}".format(round(self.capital,2)))
         print()
 
-    #TODO func to buy more bikes (order from manufacturer, get list object and merge to stock)
-    
+
+    def buy(self, bikes):
+        for bike in bikes:
+            bike.salesprice = bike.cost * (1 + self.margin/100)
+            bike.profit = bike.salesprice - bike.cost
+            bike.stock = 1 #should refer to number from bought bikes 
+            self.inventory.append(bike)
+            self.capital -= bike.cost
+   
 
 
 class Customer(object):
@@ -68,27 +112,32 @@ class Customer(object):
         self.name = name
         self.budget = budget
         self.bikes = bikes
-        
+    
         
     def can_afford(self, shop):
-        """Prints the name of customer, and a list of bikes in a given 
+        """Prints the name of customer, and a stringed list of bikes in a given 
         bike shop that the customer can afford with their budget. """
-        self.affordable = []
+        self.affordable = ""
         for bike in shop.stock:
             if bike.salesprice <= self.budget:
-                self.affordable.append(bike.model)
-        #TODO print function should return affordable as comma separated strings
-        print("From '{}' {} can afford: {}.".format(shop, self.name, self.affordable))
-        
+                self.affordable += (bike.model + ", ")
+        payable = "From '{}' {} can afford: {}.".format(shop, self.name, self.affordable)
+        print(payable.rstrip(", "))
+    
         
     def buy(self, bike, shop):
         """ Takes bike object and shop as arguments. 
         Appends bought bike to customers bike list, deducts  salesprice in shop 
         from budget and calls sell method from shop instance."""
-        
         self.bikes.append(bike.model)
         self.budget -= bike.salesprice
         print("{} just bought a {} for ${} and now has ${} left.".format(self.name, bike.model, int(bike.salesprice), int(self.budget)))
         print()
         shop.sell(bike)
+    
+        
+    def get_salary(self, salary):
+        """ Customer gets some kind of salary and now has a bigger budget for buying bikes"""
+        self.budget += salary
+        print("{} got salary and now has a budget of ${}".format(self.name, round(self.budget,2)))
 
